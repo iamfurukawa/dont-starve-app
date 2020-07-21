caeeef61d640a6400d3a8f6c719906f579aeaf4e
Vinícius Furukawa Carvalho <vinicius_furuka@hotmail.com.br>
2020-07-21 13:22:49 -0300

    Updates

---
+++ precache-manifest.2ae0cbe02c61f353f36bcb1788741e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498168fe9ab263e2420d49629f8ab697",
+    "url": "/dont-starve-app/index.html"
+  },
+  {
+    "revision": "6f3bca9fedeab810275a",
+    "url": "/dont-starve-app/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d212a6924d6d3f439b68",
+    "url": "/dont-starve-app/static/css/main.b63a97bc.chunk.css"
+  },
+  {
+    "revision": "6f3bca9fedeab810275a",
+    "url": "/dont-starve-app/static/js/2.4b5b54c3.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/dont-starve-app/static/js/2.4b5b54c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d212a6924d6d3f439b68",
+    "url": "/dont-starve-app/static/js/main.693fb95a.chunk.js"
+  },
+  {
+    "revision": "996447c32e6283579bba",
+    "url": "/dont-starve-app/static/js/runtime-main.064c9129.js"
+  }
+]);
